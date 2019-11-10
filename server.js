4c105caafca493b19ce273ed8a9eaefac563c46c
Alejandro Peinó <alepeino@gmail.com>
2019-11-10 12:29:54 -0300

    Manejador de errores

--- server.js
+++ server.js
@@ -16,4 +16,23 @@ app.get('/nuevo', (req, res) => {
   res.send(vistas.vistaForm())
 })
 
+// middleware que maneja las peticiones
+// que no matchean con las rutas declaradas.
+// genera un error, se le pasa al manejador con `next`
+app.use((req, res, next) => {
+  next({ status: 404, mensaje: 'Not found' })
+})
+
+// manejador de errores
+// último middleware
+app.use((err, req, res, next) => {
+  console.error(err)
+  const error = {
+    status: err.status || 500,
+    mensaje: err.mensaje || err.message
+  }
+  res.set('Content-Type', 'text/html')
+  res.status(err.status).send(vistas.vistaError(error))
+})
+
 app.listen(PORT, () => console.log('Server en puerto ' + PORT))
--- vistas/index.js
+++ vistas/index.js
@@ -4,6 +4,14 @@ function vistaForm () {
   return fs.readFileSync(__dirname + '/html/form.html')
 }
 
+function vistaError (error) {
+  return fs.readFileSync(__dirname + '/html/error.html')
+    .toString()
+    .replace('{{ status }}', error.status)
+    .replace('{{ mensaje }}', error.mensaje)
+}
+
 module.exports = {
+  vistaError,
   vistaForm
 }
