89f7943b0e963c4674226d11518b6f15dab19e7e
Alejandro Peinó <alepeino@gmail.com>
2019-11-10 15:27:46 -0300

    guardado de posts en FS

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const fs = require('fs')
 
 const PORT = 3000
+const POSTS_DIR = 'posts'
+
 const app = express()
 
+if (!fs.existsSync(POSTS_DIR)) {
+  fs.mkdirSync(POSTS_DIR)
+}
+
 app.set('view engine', 'ejs')
+
+// middleware que procesa el body de la request
+app.use(express.urlencoded({ extended: true }))
 
 // endpoint "home"
 app.get('/', (req, res) => {
@@ -13,6 +23,20 @@ app.get('/', (req, res) => {
 // endpoint que envía el formulario de crear post
 app.get('/nuevo', (req, res) => {
   res.render('form')
+})
+
+// endpoint que guarda el post
+app.post('/nuevo', (req, res, next) => {
+  fs.writeFile(
+    POSTS_DIR + '/' + req.body.titulo,
+    req.body.cuerpo,
+    (err, data) => {
+      if (err) {
+        return next(err)
+      }
+      res.redirect('/')
+    }
+  )
 })
 
 // middleware que maneja las peticiones
